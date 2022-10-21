6109e434165a16c1315e8a70c426c4196fd491ee
guilhermedcorrea <78979736+guilhermedcorrea@users.noreply.github.com>
2022-10-21 12:11:56 -0300

    app

--- app/controllers/controllers_notas.py
+++ app/controllers/controllers_notas.py
@@ -50,15 +50,19 @@ def emissao_nfe(f) -> Any:
     @wraps(f)
     def emissao_notas_fiscais(*args: tuple, **kwargs: Dict[str, Any]) -> Any:
         for listas in args:
-            for items in listas:
-                ValorTotal = converte_float(items.get('ValorTotal'))
-                QtdCaixa = converte_float(items.get('QtdCaixa'))
-                PrecoUnitario = converte_float(items.get('PrecoUnitario'))
-                Quantidade = converte_float(items.get('Quantidade'))
-                Desconto = converte_float(items.get('Desconto'))
-                DescontoItem = converte_float(items.get('DescontoItem'))
-
+            
+            try:
+                ValorTotal = converte_float(listas.get('ValorTotal'))
+                QtdCaixa = converte_float(listas.get('QtdCaixa'))
+                PrecoUnitario = converte_float(listas.get('PrecoUnitario'))
+                Quantidade = converte_float(listas.get('Quantidade'))
+                Desconto = converte_float(listas.get('Desconto'))
+                DescontoItem = converte_float(listas.get('DescontoItem'))
                 print('valor total -->',ValorTotal)
+            except Exception as e:
+                print(e)
+            
+            '''
                 print(items.get('CodigoPedido'), items.get('IdCliente'),items.get('NomeCliente')
                 ,items.get('SKU'),QtdCaixa,PrecoUnitario, Quantidade
                 ,items.get('EAN'), items.get('NCM'),items.get('Marca'), items.get('NomeProduto'),items.get('NomeCliente')
@@ -66,29 +70,33 @@ def emissao_nfe(f) -> Any:
                 ,items.get('RazaoSocialFranquiaVenda'),items.get('Bairro'),items.get('Celular')
                 ,items.get('Cep'),items.get('Complemento'),items.get('Endereco')
                 ,Desconto, DescontoItem,items.get('Nome'),items.get('Uf'),items.get('Numero'))
-                    
+            '''   
         print('emissao notas fiscais ')
      
         url = """https://api.nfse.io/v2/companies/{}/productinvoices/""".format(kwargs.get('COMPANY_ID_EMISSAO'))
+        try:
+            payload = json.dumps({
+            "buyer": {
+                "name": f"{listas.get('NomeCliente')}","tradeName": f"{listas.get('NomeCliente')}","address": {"city": {"code": f"{listas.get('Cep')}","name": f"{listas.get('Nome')}"
+                },"state": f"{listas.get('Uf')}","district": "distrito","street": f"{listas.get('Endereco')}","postalCode": f"{listas.get('Cep')}","number": f"{listas.get('Numero')}","country": "BRA"},
+                "federalTaxNumber": 99999999999999
+            },
+            "items": [{"code": f"{listas.get('SKU')}","unitAmount": 87.9,"quantity": f"{Quantidade}","cfop": 5102, "ncm": f"{listas.get('NCM')}","codeGTIN": f"{listas.get('EAN')}",
+                "codeTaxGTIN": f"{listas.get('EAN')}","tax": {"totalTax": 6,"icms": {"csosn": "102","origin": "0"},"pis": { "amount": 0,"rate": 0,"baseTax": 208,          "cst": "08"
+                    },"cofins": {"amount": 0,"rate": 0,"baseTax": 208,"cst": "08"}},"cest": "","description": f"{listas.get('NomeCliente')}"
+                }]})
+            headers = {
+            'Authorization': f"{kwargs.get('API_KEY_EMISSAO')}",
+            'Content-Type': 'application/json'
+            }
 
-        payload = json.dumps({
-        "buyer": {
-            "name": f"{items.get('NomeCliente')}","tradeName": f"{items.get('NomeCliente')}","address": {"city": {"code": f"{items.get('Cep')}","name": f"{items.get('Nome')}"
-            },"state": f"{items.get('Uf')}","district": "distrito","street": f"{items.get('Endereco')}","postalCode": f"{items.get('Cep')}","number": f"{items.get('Numero')}","country": "BRA"},
-            "federalTaxNumber": 99999999999999
-        },
-        "items": [{"code": f"{items.get('SKU')}","unitAmount": 87.9,"quantity": f"{Quantidade}","cfop": 5102, "ncm": f"{items.get('NCM')}","codeGTIN": f"{items.get('EAN')}",
-            "codeTaxGTIN": f"{items.get('EAN')}","tax": {"totalTax": 6,"icms": {"csosn": "102","origin": "0"},"pis": { "amount": 0,"rate": 0,"baseTax": 208,          "cst": "08"
-                },"cofins": {"amount": 0,"rate": 0,"baseTax": 208,"cst": "08"}},"cest": "","description": f"{items.get('NomeCliente')}"
-            }]})
-        headers = {
-        'Authorization': f"{kwargs.get('API_KEY_EMISSAO')}",
-        'Content-Type': 'application/json'
-        }
+            response = requests.request("POST", url, headers=headers, data=payload)
+            print(response.status_code)
+            print(f"Nota Emitida",listas['CodigoPedido'])
+        except Exception as e:
+            print(f"Nota nao Emitida ",e,listas['CodigoPedido'])
 
-        response = requests.request("POST", url, headers=headers, data=payload)
-
-        print(response.status_code)
+        
       
         #return response.json()
         return 'teste'
