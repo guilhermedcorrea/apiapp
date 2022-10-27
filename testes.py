d4032b6d9ee4443820a9e1f9ce37cace265377b6
guilhermedcorrea <78979736+guilhermedcorrea@users.noreply.github.com>
2022-10-26 23:40:58 -0300

    app

--- testes.py
+++ testes.py
@@ -1,102 +1,108 @@
-from flask import Flask
+from flask import Blueprint,make_response
+from ..extensions import db
+from sqlalchemy import text
+from itertools import groupby, chain
+import requests
+import json
 import os
-from flask_sqlalchemy import SQLAlchemy
-from config import SECRET_KEY, SQLALCHEMY_DATABASE_URI, SQLALCHEMY_BINDS
-from sqlalchemy import func, select
+from dotenv import load_dotenv
+from flask import jsonify
+testes = Blueprint('teste', __name__)
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_BINDS'] = SQLALCHEMY_BINDS
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['SQLALCHEMY_POOL_SIZE'] = 370
-app.config['SQLALCHEMY_MAX_OVERFLOW'] = 0
+
+def key_func(k):
+    return k['CodigoPedido']
 
 
-db = SQLAlchemy(app)
 
-class PedidoFlexy(db.Model):
-    __tablename__ = "PedidoFlexy"
-    __table_args__ = {"schema": "Pedidos"}
-    IdPedidoFlexy = db.Column(db.Integer, primary_key=True)
-    IdOrcamento = db.Column(db.Integer)
-    CodigoPedido = db.Column(db.Integer)
-    PrecoFrete = db.Column(db.DateTime, unique=False, nullable=False)
-    StatusPedido = db.Column(db.String)
-    IdPromocaoFlexy = db.Column(db.Integer)
-    IdCliente = db.Column(db.Integer)
-    IdColaborador = db.Column(db.Integer)
-    Comissao = db.Column(db.DateTime, unique=False, nullable=False)
-    IdUnidade = db.Column(db.Integer)
-    IdFormaPagamento = db.Column(db.Integer)
-    IdAtributo = db.Column(db.Integer)
-    ValorTotal = db.Column(db.DateTime, unique=False, nullable=False)
-    IdEtapaFlexy = db.Column(db.Integer)
-    Desconto = db.Column(db.DateTime, unique=False, nullable=False)
-    ValorEntrada = db.Column(db.DateTime, unique=False, nullable=False)
-    DataInserido = db.Column(db.DateTime, unique=False, nullable=False)
-    BitSplit = db.Column(db.Boolean, unique=False, nullable=False)
-    BitOmie = db.Column(db.Boolean, unique=False, nullable=False)
-    CodPedidoOmie = db.Column(db.String)
-    NumPedidoOmie = db.Column(db.String)
-    ValorTotalDescontado = db.Column(db.Float)
-    Split = db.Column(db.Float)
-    Margem =  db.Column(db.Float)
-    WmsEtapa = db.Column(db.Integer)
-    DataInseridoOmie = db.Column(db.DateTime, unique=False, nullable=False)
-    NumPedidoFornecedor = db.Column(db.String)
-    PedidoPai = db.Column(db.Integer)
-    PrevisaoEntrega = db.Column(db.DateTime, unique=False, nullable=False)
-    bitDesconto = db.Column(db.Boolean, unique=False, nullable=False)
-    bitAtualizadoStatus = db.Column(db.Boolean, unique=False, nullable=False)
-    NomePedido = db.Column(db.String)
-    OrigemPedido = db.Column(db.String)
-    DesmPedido = db.Column(db.Integer)
-    IdEspecialista = db.Column(db.Integer)
-    IdOrderJet = db.Column(db.Integer)
-    IdSistema = db.Column(db.Integer)
-    IdCardJestor = db.Column(db.Integer)
-    PrevisaoOriginal  = db.Column(db.DateTime, unique=False, nullable=False)
+load_dotenv()
+
+API_KEY_EMISSAO = os.getenv('API_KEY_EMISSAO')
+COMPANY_ID_EMISSAO = os.getenv('COMPANY_ID_EMISSAO')
 
 
-'''
-def testes():
-    with db.engine.connect() as conn:
-        transactions_data = conn.execute(select(func.strftime('%', PedidoFlexy.DataInserido)
-                    ,func.sum(PedidoFlexy.DataInserido)).group_by()).all()
-        print(transactions_data)
-testes()
-'''
+def cadastrar_nfe(*args, **kwargs):
+    url = "https://api.nfse.io/v2/companies/acd0c1c8f5a1486592c6ed80d94e2bb7/productinvoices/"
 
+    payload = json.dumps({"buyer": {"name": f"{kwargs.get('name_cliente')}","tradeName": "Comprador Nome Comercial",
+        "address": {"city": {"code": "1504018","name": f"{kwargs.get('name_city')}"},"state": "SP","district": "distrito",
+        "street": f"{kwargs.get('street')}","postalCode": f"{kwargs.get('postalCode')}","number": f"{kwargs.get('postalCode')}",
+        "country": "BRA"},"federalTaxNumber": 99999999999999
+    },"items": kwargs.get('items')})
+    headers = {
+    'Authorization': 't0StUhoH4JiSN72ehwrhq3nQ27gRDTSJGt2W98rDXilRTwhNoJAiGtM9WUcl9MscjjW',
+    'Content-Type': 'application/json'
+    }
 
-'''
+    response = requests.request("POST", url, headers=headers, data=payload)
 
-        query = (text("""
-            SELECT DISTINCT *FROM [HauszMapa].[Pedidos].[PedidoFlexy] AS PFLEXY
-            JOIN [HauszMapa].[Pedidos].[EnderecoPedidos]AS EPEDIDO
-            ON EPEDIDO.Idcliente = PFLEXY.IdCliente
-            JOIN [HauszMapa].[Cadastro].[Cidade] as ccidade
-            ON ccidade.IdCidade = EPEDIDO.IdCidade
-            JOIN [HauszMapa].[Cadastro].[Estado] as cestado
-            ON cestado.IdEstado = ccidade.IdEstado
-            WHERE convert(date,PFLEXY.[DataInserido])  =  '2022-10-18'
-            AND PFLEXY.StatusPedido ='Em separação'"""))
-        teste = conn.execute(query).all()
-      
-        query_dicts = [{key: value for (key, value) in row.items()} for row in teste]
-        for pedidos in query_dicts:
-          
-            jsons = executa_select(pedido = pedidos.get('CodigoPedido'))
-            dict_items = next(chain(jsons))
-            #print(dict_items)
-            dict_items = sorted(dict_items, key=group_keys)
-            for key, value in groupby(dict_items, group_keys):
-                #cont = len(list(value))
-                dicts = verifica_dict(list(value))
-                dicts.update(pedidos)
-                lista_pedidos.append(dicts)
+    response.status_code
+    return response.status_code
+  
+@testes.route("/testes", methods=['GET','POST'])
+def teste():
     
-    emissao_nf(lista_pedidos,API_KEY_EMISSAO,COMPANY_ID_EMISSAO)
-    return make_response(jsonify({'EmitindoNFE':lista_pedidos})),201
-'''
+    '''
+    cidade_pv =  dict_pedidos['buyer']['address']
+
+    pv_produto = dict_pedidos['items'][0]
+    tax = dict_pedidos['items'][0]['tax']
+    '''
+
+    with db.engine.connect() as conn:
+            query = (text("""SELECT *FROM [HauszMapa].[Pedidos].[ItensFlexy] as iflexy
+                JOIN [HauszMapa].[Pedidos].[PedidoFlexy] as pflexy
+                ON iflexy.CodigoPedido = pflexy.CodigoPedido
+                JOIN [HauszMapa].[Produtos].[ProdutoBasico] as pbasico
+                ON pbasico.SKU = iflexy.SKU
+                JOIN [HauszLogin].[Cadastro].[Cliente] as cliente
+                ON cliente.IdCliente = pflexy.IdCliente
+                JOIN [HauszMapa].[Pedidos].[EnderecoPedidos] as epedido
+                ON epedido.IdCliente = cliente.IdCliente
+                JOIN [HauszLogin].[Cadastro].[Cidade] as cidade
+                ON cidade.IdCidade = epedido.IdCidade
+                JOIN [HauszLogin].[Cadastro].[Estado] as estado
+                ON estado.IdEstado = cidade.IdEstado
+                WHERE iflexy.CodigoPedido = 70829 """))
+                     
+            teste = conn.execute(query).all()
+            query_dicts = [{key: value for (key, value) in row.items()} for row in teste]
+            INFO = sorted(query_dicts, key=key_func)
+            lista_values = []
+            lista_item = []
+            buyer = {}
+            for key, value in groupby(INFO, key_func):
+                lista = list(value)
+
+                for i in range(len(lista)):
+
+                    buyer['namecliente'] = lista[i].get('NomeCliente')
+                    buyer['tradeName'] = lista[0].get('NomeCliente')
+                    buyer['code'] = lista[i].get('Cep')
+                    buyer['name'] = lista[i].get('Nome')
+                    buyer['street'] = lista[i].get('Endereco')
+                    buyer['number'] = lista[i].get('Numero')
+                    buyer['postalCode'] = lista[i].get('Cep')
+                    buyer['description'] = lista[i]['CodigoPedido']
+                    buyer['CodigoPedido'] = lista[i]['CodigoPedido']
+
+                  
+                    new_item = {"code": f"{lista[i]['SKU']}","unitAmount": float(lista[i]['PrecoUnitario']), "quantity": float(lista[i]['Quantidade'])
+                    ,"cfop": 5102,"ncm": f"{lista[i]['NCM']}",
+                    "codeGTIN": f"{lista[i]['EAN']}","codeTaxGTIN": f"{lista[i]['EAN']}", "tax": {"totalTax": 6, "icms": {
+                    "csosn": "102","origin": "0"},"pis": {"amount": 0,"rate": 0,"baseTax": 208,"cst": "08" },
+                    "cofins": {"amount": 0,"rate": 0,"baseTax": 208,"cst": "08"}},"cest": "",
+                    "description": f"{lista[i].get('NomeProduto')}"}
+    
+                    
+                    lista_item.append(new_item)
+                
+            jsons = cadastrar_nfe(items = lista_item, name_cliente=buyer.get('namecliente'), tradeName=buyer.get('tradeName'),code=buyer.get('code')
+                ,name_city=buyer.get('name'),street=buyer.get('street'),number=buyer.get('number')
+                    ,postalCode=buyer.get('postalCode'),description=buyer.get('description'),id=buyer.get('CodigoPedido'))
+
+         
+                 
+    return make_response(jsonify({"NotaFiscal":"Criada Com Sucesso"
+        ,"Status":200,"Pedido":buyer['CodigoPedido'],"Cliente":buyer['namecliente'],"item":jsons})),201
