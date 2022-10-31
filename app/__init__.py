f661c5c98cef576cf5b1445df367a2a0f2dd856e
root <root@debian.debian>
2022-10-31 15:41:18 -0300

    app

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
 from config import SECRET_KEY, SQLALCHEMY_DATABASE_URI, SQLALCHEMY_BINDS
-from .extensions import db
+from .extensions import db, ma
+from apifairy import APIFairy
 
+apifairy = APIFairy()
 
 def create_app() -> Flask:
     app = Flask(__name__, template_folder='templates')
@@ -14,8 +16,12 @@ def create_app() -> Flask:
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config['SQLALCHEMY_POOL_SIZE'] = 370
     app.config['SQLALCHEMY_MAX_OVERFLOW'] = 0
-
+    
     db.init_app(app)
+    app.config['APIFAIRY_TITLE'] = 'NFE-IO'
+    app.config['APIFAIRY_VERSION'] = '1.0'
+    apifairy.init_app(app)
+    ma.init_app(app)
  
     with app.app_context():
         """BluePrints
--- app/api/consulta_nf.py
+++ app/api/consulta_nf.py
@@ -5,10 +5,10 @@ import os
 from dotenv import load_dotenv
 import requests
 from typing import Dict, Tuple, List, Literal
-from ..extensions import db
+from ..extensions import db, ma
 from ..models.pedidos_hausz_mapa import PedidoFlexy
 from ..models.cliente_hausz_mapa import EnderecoPedidos
-
+from apifairy import response, other_responses, body, arguments, authenticate
 from sqlalchemy import text
 from datetime import datetime
 from itertools import chain
@@ -43,6 +43,8 @@ def register_handlers(app):
         return make_response(jsonify({"Error":"Endpoint NotFound"}), 405)
 
 consulta_bp = Blueprint('consultanf', __name__)
+
+
 register_handlers(current_app)
 
 from ..controllers.controllers_notas import get_metodo, list_all_empresas
@@ -70,7 +72,20 @@ Lista notas
 https://nfe.io/docs/desenvolvedores/rest-api/nota-fiscal-de-consumidor-v2/#/
 """
 
-@consulta_bp.route('/api/v2/companies/list/empresas/all', methods=['GET','POST'])
+class ListCompaniesAll(ma.Schema):
+    ID = ma.Int()
+    taxRegime = ma.Str()
+    address = ma.Str()
+    modifiedOn = ma.Str()
+    createdOn = ma.Str()
+    status = ma.Str()
+    federalTaxNumber = ma.Str()
+    name = ma.Str()
+    accountId = ma.Str()
+    
+
+@consulta_bp.route('/api/v2/companies/list/empresas/all', methods=['GET'])
+@response(ListCompaniesAll, 201)
 def listas_all_empresas() -> Response:
     """Retorna lista de empresas cadastradas"""
     try:
@@ -95,8 +110,16 @@ def listas_all_empresas() -> Response:
     except:
         abort(400)
 
+class CompaniesListNfeAll(ma.Schema):
+    serie = ma.Str()
+    number = ma.Str()
+    issuer = ma.Str()
+    buyer = ma.Str()
+    totals = ma.Str()
+    
 
 @consulta_bp.route('/api/v2/companies/list/nfe/all', methods=['GET','POST'])
+@response(CompaniesListNfeAll, 201)
 def consulta_nf() -> Response:
     """Retorna todas as notas fiscais emitidas"""
     try:
@@ -115,30 +138,41 @@ def consulta_nf() -> Response:
         return make_response(jsonify(lista_notas))
     except:
         abort(400)
+        
+'''
+[{'companyId': 'acd0c1c8f5a1486592c6ed80d94e2bb7',
+'id': '223b5879af86407485ed8efd4f929f9f', 'taxUnitAmount': 9.98,
+'quantityTax': 5.0, 'totalAmount': 49.9, 'unitAmount': 9.98, 'quantity': 5.0, 
+'totalIndicator': True, 'cfop': 5102, 'tax': {'totalTax': 6.0,
+'icms': {'baseTaxFCPSTAmount': 0.0, 'fcpstRetAmount': 0.0, 'fcpstRetRate': 0.0, 
+'fcpstAmount': 0.0, 'fcpstRate': 0.0, 'snCreditAmount': 0.0, 'snCreditRate': 0.0,
+'csosn': '102', 'origin': '0'}, 'pis': {'amount': 0.0, 'rate': 0.0, 'baseTax': 208.0, 'cst': '08'}, 'cofins': {'amount': 0.0, 'rate': 0.0, 'baseTax': 208.0, 'cst': '08'}}, 'cest': '', 'unitTax': 'UN', 'unit': 'UN', 'ncm': '47079000',
+'description': 'TESTE DE PRODUTO - WITMOB', 'codeGTIN': 'SEM GTIN', 'code': '001'}]
 
 
+'''      
+class CompaniesListProdutosNfe(ma.Schema):
+    id = ma.Str()
+    companyId = ma.Str()
+  
 @consulta_bp.route('/api/v2/companies/list/produtos/nfe', methods=['GET','POST'])
 def retorna_produtos_nfe():
     idnota = request.get_json()
     ref_nota = idnota['IdNota']
+    lista_dicts = []
     jsons = lista_produtos_nfe(ref_nota)
+    print(jsons)
+    for keys in jsons.get('items'):
+        dicts = {}
+        dicts['companyId'] = jsons.get('companyId')
+        dicts['id'] = jsons.get('id')
+        dicts.update(keys)
+        lista_dicts.append(dicts)
     
-    dict_produtos = [item for item in jsons['items']]
-    for items in dict_produtos:
-        print(items)
-  
-  
-    """
-    hasMore
-items
-accountId
-companyId
-id
-    """
-    for x in jsons:
-        print(x)
+    return make_response(jsonify(lista_dicts)), 201
+       
+    
 
-    return jsons
 
 @consulta_bp.route('/api/v2/companies/list/nfe/idnfe', methods=['GET','POST'])
 def retorna_nfe_id():
